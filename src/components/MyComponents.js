61e36a25faee6d94fa09288d37ad6f2976c64e72
ChuHuy <chuhuy11042001@gmail.com>
2025-03-14 09:40:16 +0700

    setState - update state của component

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -6,16 +6,19 @@ class MyComponent extends React.Component {
     state = {
         name: "Huy",
         add: "Hanoi",
+        age: 24,
     };
 
     handleClick = (event) => {
-        alert("Hello World!");
-        //console.log(event.pageX);
+        console.log("Heello", this.state.name);
+        this.setState({
+            name: "Huy Chu",
+            age: Math.floor(Math.random() * 100 + 1),
+        });
     };
 
     handleOnMouseOver = (event) => {
-        console.log(this.state.name);
-        console.log(this.state.add);
+        alert("Hello World!");
     };
 
     render() {
@@ -24,8 +27,9 @@ class MyComponent extends React.Component {
                 <h1>First component!!!</h1>
                 My name is {this.state.name} <br />
                 My address is {this.state.add} <br />
-                <button onMouseOver={this.handleOnMouseOver}>Click me!</button>
-                <button onClick={this.handleClick}>Alert</button>
+                My age is {this.state.age} <br />
+                {/* <button onMouseOver={this.handleOnMouseOver}>Alert!</button> */}
+                <button onClick={this.handleClick}>Click!</button>
             </div>
         );
     }

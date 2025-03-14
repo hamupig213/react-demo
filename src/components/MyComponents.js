0c87c52a4751fa1c5bbc26fe2a38d238ff6ac9f1
ChuHuy <chuhuy11042001@gmail.com>
2025-03-14 09:02:58 +0700

    state trong react

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -3,12 +3,17 @@
 import React from "react";
 
 class MyComponent extends React.Component {
-    //JSX
+    trangThai = {
+        name: "Huy",
+        add: "Hanoi",
+    };
+
     render() {
         return (
             <div>
                 <h1>First component!!!</h1>
-                {Math.random()}
+                My name is {this.trangThai.name} <br/>
+                My address is {this.trangThai.add}
             </div>
         );
     }
